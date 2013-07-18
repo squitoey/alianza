1f0a3f22c52026a5b3b561bf40f5dda6b98beec5
Dallin Jones <me@dallinjones.com>
2013-07-18 09:45:54 -0600

    Designing the parser.

--- src/main/java/com/alianza/dallin/util/Parser.java
+++ src/main/java/com/alianza/dallin/util/Parser.java
@@ -8,8 +8,37 @@ import java.util.List;
  * Time: 10:54 PM
  */
 public class Parser {
+    /**
+     * The instructions said not to use any prepackaged Arrays, or Lists, but that we could use a library for System I/O
+     * since java.nio work in a collection of strings, we take that as input, but we do not do anything else with it,
+     * other than iterate over the collection so that we can get the data.
+     *
+     * The idea.... input stream is as follow:
+     * 6, 1, 54, 12, 22, 1, 6, 1
+     *
+     *
+     * 6
+     * 1, 6
+     * 1, 6, 54
+     * 1, 6, 12, 54
+     * So we create an array that is 8 elements long (the length of the input)
+     * -- -- -- -- -- -- -- --
+     * -- -- -- -6 -- -- -- --
+     * -- -1 -- -- -- -- -- --
+     * -- -- -- -- -- 54 -- --
+     * -- -- -- -- 12 -- -- --
+     * -- -- -- -- -- 22 54 --
+     * -1 -- -- -- -- -- -- --
+     * -- -- -6 -- -- -- -- --
+     * -- -- -- -- -- -- -- -1
+     * Needed data? Middle, start, end
+     * Find the middle of
+     * @param strings
+     * @return
+     */
     public int[][] sortStrings(List<String> strings) {
-        int[][] sortedIntegers = new int[1][2];
+        int length = strings.size();
+        int [][] sortedIntegers = new int[length][2];
 
         return sortedIntegers;
     }
