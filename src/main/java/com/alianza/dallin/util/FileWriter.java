bbac5aad5d2daa175799543b34b247d048962e6d
Dallin Jones <me@dallinjones.com>
2013-07-25 23:15:09 -0600

    Refactoring File writing to a utility class for reduced code.
    + Added a FileWriter class to simplify things.
    + Added a gradle task to do everything (ie. compile, and run the app)

---
+++ src/main/java/com/alianza/dallin/util/FileWriter.java
@@ -0,0 +1,11 @@
+package com.alianza.dallin.util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Dallin
+ * Date: 7/25/13
+ * Time: 11:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FileWriter {
+}
